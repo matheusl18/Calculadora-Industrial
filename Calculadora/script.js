5a5bbf5f3b5bd928bf4448dfd142258443340bb3
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2023-01-06 18:30:35 -0300

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,17 +1,53 @@
-let btn = document.getElementById("calcular");
+let btn = document.querySelector('#calcular');
+let resultados = document.querySelector("#resultados");
+let pesomil = document.querySelector("#pesomil");
+let pesototal = document.querySelector("#pesototal");
 
 btn.addEventListener('click', () => {
     var largura = document.getElementById("largura").value;
     var altura = document.getElementById("altura").value;
     var espessura = document.getElementById("espessura").value;
-    var quantidade = document.getElementById("quantidade").value;
+    var quantidade = 1000.0;
+    var material = document.getElementById("material");
     var fator = 0.1;
+    let modelo = document.getElementById("modelo").selectedIndex;
+    var cores = document.getElementById("cores").value;
 
-    var pesoMil = largura*altura*espessura*fator;
+    var materialOption = material.options[material.selectedIndex].text;
+    var materialSplit = materialOption.split("-");
+    var materialValor = materialSplit[1];
+    var pesoMil = largura*altura*espessura*fator*materialValor;
     var pesoTotal = pesoMil*quantidade;
-    var qtdmin = pesoTotal/pesoMil;
-    alert(pesoMil);
-    alert(pesoTotal);
-    alert(qtdmin);
+    if (pesoTotal < 100 && modelo == 1 || pesoTotal < 100 && modelo == 0 && cores <= 4){
+        alert("Peso total não atingiu o minimo de 100");
+        resultados.value = 0;
+        pesomil.value = 0;
+        pesototal.value = 0;
+    } else if(pesoTotal < 300 && modelo == 0 && cores <= 6){
+        alert("Peso total não atingiu o minimo de 300");
+        resultados.value = 0;
+        pesomil.value = 0;
+        pesototal.value = 0;
+    } else{
+        var qtdmin = pesoTotal/pesoMil;
+        resultados.value = qtdmin;
+        pesomil.value = pesoMil;
+        pesototal.value = pesoTotal;
+        /*alert(pesoMil);
+        alert(pesoTotal);
+        alert(qtdmin);*/
+    };
+});
 
-})
+function colors(){
+    let modelo = document.getElementById("modelo").selectedIndex;
+    let cores = document.getElementById("cores");
+    if (modelo == 1){
+        cores.readOnly = true;
+        cores.style.pointerEvents = "none";
+        cores.value = '1';
+    }else{
+        cores.readOnly = false;
+        cores.style.pointerEvents = "fill";
+    };
+};
