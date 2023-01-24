9f967c112f3883400e773ee2f5f39d4c407286b4
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2023-01-24 19:54:14 -0300

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -3,13 +3,23 @@ let resultados = document.querySelector("#resultados");
 let pesomil = document.querySelector("#pesomil");
 let pesototal = document.querySelector("#pesototal");
 
+
 const inputs = document.querySelectorAll("input");
 inputs.forEach(input => {
-    input.addEventListener('change', function(){
-        this.value = parseFloat(this.value).toFixed(2);
-    });
+    if(input.id == "espessura"){
+        input.addEventListener('change', function(){
+            this.value = parseFloat(this.value).toFixed(3);
+        });
+    } else{
+        input.addEventListener('change', function(){
+            if (this.value < 4){
+                alert(`O valor minimo da ${input.id} é 4`)
+                this.value = 4;
+            }
+            this.value = parseFloat(this.value).toFixed(2);
+        });
+    };
 });
-
 
 btn.addEventListener('click', () => {
     var largura = document.getElementById("largura").value;
@@ -22,44 +32,40 @@ btn.addEventListener('click', () => {
     var cores = document.getElementById("cores").value;
 
     var materialOption = material.options[material.selectedIndex].text;
-    if (materialOption == "PEBD"){
-        materialValor = 0.92
-    } else if (materialOption == "PEAD"){
-        materialValor = 0.95
-    } else if (materialOption == "PP"){
-        materialValor = 0.90
-    } else if (materialOption == "BOPP TRANSP"){
-        materialValor = 0.91
-    } else if (materialOption == "BOPP PEROLA"){
-        materialValor = 0.70
-    } else if (materialOption == "NYLON"){
-        materialValor = 1.0
+    switch (materialOption){
+        case "PEBD":
+            materialValor = 0.92;
+            break;
+        case "PEAD":
+            materialValor = 0.95;
+            break;
+        case "PP":
+            materialValor = 0.90;
+            break;
+        case "BOPP TRANSP":
+            materialValor = 0.91;
+            break;
+        case "BOPP PEROLA":
+            materialValor = 0.70;
+            break;
+        case "NYLON":
+            materialValor = 1.0;
+            break;
     }
     var pesoMil = largura*altura*espessura*fator*materialValor;
     var pesoTotal = pesoMil*quantidade;
     if (pesoTotal < 100 && modelo == 1 || pesoTotal < 100 && modelo == 0 && cores <= 4){
         alert("Peso total não atingiu o minimo de 100 kg");
         resultados.value = 0;
-        /*pesomil.value = 0;
-        pesototal.value = 0;*/
     } else if(pesoTotal > 100 && modelo == 1 || pesoTotal > 100 && modelo == 0 && cores <= 4){
         var qtdmin = 100/pesoMil;
         resultados.value = parseFloat(qtdmin).toFixed(3);
-       /* pesomil.value = pesoMil;
-        pesototal.value = pesoTotal;*/
     } else if(pesoTotal < 300 && modelo == 0 && cores <= 6){
         alert("Peso total não atingiu o minimo de 300 kg");
         resultados.value = 0;
-        /*pesomil.value = 0;
-        pesototal.value = 0;*/
     } else if(pesoTotal > 300 && modelo == 0 && cores <= 6){
         var qtdmin = 300/pesoMil;
         resultados.value = parseFloat(qtdmin).toFixed(3);
-        /*pesomil.value = pesoMil;
-        pesototal.value = pesoTotal;*/
-        /*alert(pesoMil);
-        alert(pesoTotal);
-        alert(qtdmin);*/
     };
 });
 
